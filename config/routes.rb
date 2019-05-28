542fc9444f67ce0ab82d5f4f4c15bc5e8ffd7150
david <david@david-rMBP.localdomain>
2019-05-28 13:32:17 +0100

    initial route

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   
   namespace :admin do
     root 'panel#index'
-<<<<<<< HEAD
-    resources :cupboards
-    resources :assets
-=======
     devise_for :users, :module => "devise"
     resources :users, only: [:index, :show]
 
@@ -15,7 +11,6 @@ Rails.application.routes.draw do
       resources :assets
     end
     
->>>>>>> develop
   end
 
 end
