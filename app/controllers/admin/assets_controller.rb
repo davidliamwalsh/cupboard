7b900887ef41bc036693d8679aefed87b47f290c
David Walsh <davidliamwalsh@yahoo.co.uk>
2019-05-29 13:36:42 +0100

    partial commit (#8)

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,13 +1,18 @@
 class Admin::AssetsController < Admin::ApplicationController
 
   def create
-    @asset = Asset.new(asset_params)
-    @asset.cupboard_id = params[:cupboard_id]
-
+    @cupboard = Cupboard.find(params[:cupboard_id])
+    @asset = @cupboard.assets.build(asset_params)
+    
     @asset.save
     
     redirect_to admin_cupboard_path(@asset.cupboard)
   end
+
+  def destroy
+  end
+
+  private
 
   def asset_params
     params.require(:asset).permit(:title, :sku)
